d838b99beafb24e15488b9e87fb952e0191b0a5f
the-Astra <djs8743@rit.edu>
2026-09-10 00:43:19 -0400

    use icon_path metadata

--- ArtBox.lua
+++ ArtBox.lua
@@ -76,13 +76,6 @@ end
 
 --#region Atlases
 SMODS.Atlas {
-    key = 'modicon',
-    px = 34,
-    py = 34,
-    path = 'modicon.png'
-}
-
-SMODS.Atlas {
     key = "joker_atlas",
     path = "jokers.png",
     px = 71,
