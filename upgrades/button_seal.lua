5120227c14e9bd1c0ad6663455742bad1a762a85
the-Astra <djs8743@rit.edu>
2026-09-14 17:19:34 -0400

    button seal refactor

--- upgrades/button_seal.lua
+++ upgrades/button_seal.lua
@@ -1,38 +1,35 @@
 SMODS.Seal {
-    key = "button",
-    badge_colour = HEX("4f6367"),
-    atlas = "seal_atlas",
-    pos = {x= 3, y= 0},
-    discovered = true,
+  key = "button",
+  badge_colour = HEX("4f6367"),
+  atlas = "seal_atlas",
+  pos = { x = 3, y = 0 },
+  discovered = true,
 
-    calculate = function(self, card, context)
-
-      if context.cardarea == G.play and context.main_scoring then
-        local cards = {}
-        local valid_targets
-        if G.hand and G.hand.cards and #G.hand.cards > 0 then
-            for k, v in ipairs(G.hand.cards) do
-            if not v.base.suit~=card.base.suit then 
-                valid_targets=true
-            end
-        end
-        else
-            valid_targets=false
+  calculate = function(self, card, context)
+    if context.cardarea == G.play and context.main_scoring then
+      local valid_targets = {}
+      if G.hand and G.hand.cards and #G.hand.cards > 0 then
+        for _, v in ipairs(G.hand.cards) do
+          if v.base.suit ~= card.base.suit and not v['artb_button_queued_'..card.base.suit] then
+            table.insert(valid_targets, v)
+          end
         end
 
-        if valid_targets==true then
-          for k, v in ipairs(G.hand.cards) do
-            if v.base.suit~=card.base.suit then cards[#cards + 1] = v end
+        if next(valid_targets) then
+          local _card = pseudorandom_element(valid_targets, pseudoseed('button'))
+          _card['artb_button_queued_'..card.base.suit] = true
+          G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.1,
+            func = function()
+              assert(SMODS.change_base(_card, card.base.suit))
+              _card:juice_up(0.3, 0.5)
+              _card['artb_button_queued_'..card.base.suit] = nil
+              return true
             end
-            local _card = pseudorandom_element(cards, pseudoseed('button'))
-            if _card~=nil then
-              G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.1, func = function()
-                SMODS.change_base(_card, card.base.suit,nil)
-                _card:juice_up(0.3, 0.5)
-              return true end }))
-            end
-          
+          }))
         end
       end
     end
+  end
 }
